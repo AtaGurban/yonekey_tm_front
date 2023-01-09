b44db9dfa0ac3cca778b4400775bec76acbbec3c
Ata Gurban <ata-g@mail.ru>
2023-01-09 17:05:54 +0500

    fd

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { MoonLoader } from "react-spinners";
-import { clickBusiness, getAllSlider, getBusiness } from "../../http/mainPageApi";
+import {
+  clickBusiness,
+  getAllSlider,
+  getBusiness,
+} from "../../http/mainPageApi";
 import MainNavbar from "./components/MainNavbar";
 import styles from "./main.module.css";
 import Slider from "./components/Slider";
@@ -8,6 +12,7 @@ import { Context } from "../..";
 import ProductCategory from "./components/ProductCategory";
 import Popup from "../../components/Popup";
 import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
 
 const Main = observer(() => {
   const [loading, setLoading] = useState(true);
@@ -17,22 +22,35 @@ const Main = observer(() => {
   const clickBusinessFunc = async (id) => {
     await clickBusiness(id);
   };
-  const firstTitleCategory = category.titleCategory.filter((i)=> i.number === 1)
-  const secondTitleCategory = category.titleCategory.filter((i)=> i.number === 2)
-  const thirdTitleCategory = category.titleCategory.filter((i)=> i.number === 3)
-  const fourTitleCategory = category.titleCategory.filter((i)=> i.number === 4)
-  const fiveTitleCategory = category.titleCategory.filter((i)=> i.number === 5)
-  const sixTitleCategory = category.titleCategory.filter((i)=> i.number === 6)
+  const firstTitleCategory = category.titleCategory.filter(
+    (i) => i.number === 1
+  );
+  const secondTitleCategory = category.titleCategory.filter(
+    (i) => i.number === 2
+  );
+  const thirdTitleCategory = category.titleCategory.filter(
+    (i) => i.number === 3
+  );
+  const fourTitleCategory = category.titleCategory.filter(
+    (i) => i.number === 4
+  );
+  const fiveTitleCategory = category.titleCategory.filter(
+    (i) => i.number === 5
+  );
+  const sixTitleCategory = category.titleCategory.filter((i) => i.number === 6);
   useEffect(() => {
     (async function () {
-      await getBusiness()
+      await getBusiness().then(async (data) => {
+        setBusiness(data);
+      });
+
+      await getAllSlider()
         .then(async (data) => {
-          setBusiness(data);
+          setSliders(data);
         })
-        
-      await getAllSlider().then(async (data) => {
-        setSliders(data);
-      }).finally(() => {setLoading(false)});
+        .finally(() => {
+          setLoading(false);
+        });
     })();
   }, []);
 
@@ -59,55 +77,86 @@ const Main = observer(() => {
             .sort((a, b) => {
               return b.counter - a.counter;
             })
-            .map((i) => (
-              <a
-                key={i.id}
-                onClick={() => clickBusinessFunc(i.id)}
-                href={i.link}
-              >
-                <div className={`${styles["business-box"]} mx-2 my-2`}>
-                  {i.name}
-                </div>
-              </a>
-            ))}
+            .map((i) =>
+              i.name === "Yonekey" || i.name === "YBilim" ? (
+                <a
+                  key={i.id}
+                  onClick={() => clickBusinessFunc(i.id)}
+                  href={i.link}
+                >
+                  <div className={`${styles["business-box"]} mx-2 my-2`}>
+                    {i.name}
+                  </div>
+                </a>
+              ) : (
+                <Link
+                  key={i.id}
+                  onClick={() => clickBusinessFunc(i.id)}
+                  to={i.link}
+                >
+                  <div className={`${styles["business-box"]} mx-2 my-2`}>
+                    {i.name}
+                  </div>
+                </Link>
+              )
+            )}
         </div>
-        <Slider slider={(sliders.filter(i=> i.number === 1))[0]}/>
-        <div className={`${styles["our-business"]} container my-2 w-100 text-center`}>
+        <Slider slider={sliders.filter((i) => i.number === 1)[0]} />
+        <div
+          className={`${styles["our-business"]} container my-2 w-100 text-center`}
+        >
           <h2 className="fw-bold fs-1">Biziň Bisnezlerimiz</h2>
           <div className={`${styles["slide-business-two"]} my-3 w-100 d-flex`}>
             {business
               .sort((a, b) => {
                 return b.counter - a.counter;
               })
-              .map((i) => (
-                <a
-                  onClick={() => clickBusinessFunc(i.id)}
-                  key={i.id}
-                  className={`${styles["business-box-two"]} my-2 mx-4`}
-                  href={i.link}
-                >
-                  <div className={`${styles["business-box-two"]} `}>
-                    <img
-                      // style={{ height: 250 }}
-                      src={`${process.env.REACT_APP_API_URL}api/static/${i.img}`}
-                      alt=""
-                    />
-                  </div>
-                </a>
-              ))}
+              .map((i) =>
+                (i.name === "Yonekey" || i.name === "YBilim") ? (
+                  <a
+                    onClick={() => clickBusinessFunc(i.id)}
+                    key={i.id}
+                    className={`${styles["business-box-two"]} my-2 mx-4`}
+                    href={i.link}
+                  >
+                    <div className={`${styles["business-box-two"]} `}>
+                      <img
+                        // style={{ height: 250 }}
+                        src={`${process.env.REACT_APP_API_URL}api/static/${i.img}`}
+                        alt=""
+                      />
+                    </div>
+                  </a>
+                ) : (
+                  <Link
+                    onClick={() => clickBusinessFunc(i.id)}
+                    key={i.id}
+                    className={`${styles["business-box-two"]} my-2 mx-4`}
+                    to={i.link}
+                  >
+                    <div className={`${styles["business-box-two"]} `}>
+                      <img
+                        // style={{ height: 250 }}
+                        src={`${process.env.REACT_APP_API_URL}api/static/${i.img}`}
+                        alt=""
+                      />
+                    </div>
+                  </Link>
+                )
+              )}
           </div>
         </div>
-        <Slider slider={(sliders.filter(i=> i.number === 2))[0]}/>
-        <ProductCategory titleCategory={firstTitleCategory[0]}/>
-        <ProductCategory titleCategory={secondTitleCategory[0]}/>
-        <Slider slider={(sliders.filter(i=> i.number === 3))[0]}/>
-        <ProductCategory titleCategory={thirdTitleCategory[0]}/>
-        <Slider slider={(sliders.filter(i=> i.number === 4))[0]}/>
-        <ProductCategory titleCategory={fourTitleCategory[0]}/>
-        <Slider slider={(sliders.filter(i=> i.number === 5))[0]}/>
-        <ProductCategory titleCategory={fiveTitleCategory[0]}/>
-        <Slider slider={(sliders.filter(i=> i.number === 6))[0]}/>
-        <ProductCategory titleCategory={sixTitleCategory[0]}/>
+        <Slider slider={sliders.filter((i) => i.number === 2)[0]} />
+        <ProductCategory titleCategory={firstTitleCategory[0]} />
+        <ProductCategory titleCategory={secondTitleCategory[0]} />
+        <Slider slider={sliders.filter((i) => i.number === 3)[0]} />
+        <ProductCategory titleCategory={thirdTitleCategory[0]} />
+        <Slider slider={sliders.filter((i) => i.number === 4)[0]} />
+        <ProductCategory titleCategory={fourTitleCategory[0]} />
+        <Slider slider={sliders.filter((i) => i.number === 5)[0]} />
+        <ProductCategory titleCategory={fiveTitleCategory[0]} />
+        <Slider slider={sliders.filter((i) => i.number === 6)[0]} />
+        <ProductCategory titleCategory={sixTitleCategory[0]} />
       </div>
     </div>
   );
